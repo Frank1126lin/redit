87bd43edac3ee3150070d491eee20117ba10f053
frank <frank1126.lin@gmail.com>
2024-07-31 08:49:28 +0800

    test zed editor

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![allow(non_snake_case)]
 #![allow(unused_assignments)]
 
-// use anyhow::{anyhow, Result};
+use anyhow::{anyhow, Result};
 // use std::path::{Path, PathBuf};
 // use std::rc::Rc;
 
@@ -10,12 +10,11 @@
 // use toml::Value;
 // use tracing::{debug, error, info, warn};
 
-
 // use chrono::{Local, Timelike, Duration};
 
 slint::include_modules!();
 
-fn main()-> Result<(), slint::PlatformError> {
+fn main() -> Result<(), slint::PlatformError> {
     //日志初始化
     // let file_appender = tracing_appender::rolling::daily("./log", "tracing.log");
     // let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
@@ -23,27 +22,25 @@ fn main()-> Result<(), slint::PlatformError> {
     // 根据输入的文档路径，加载文档内容，并写入到编辑器中
     let ui = AppWindow::new()?;
 
-    ui.on_request_increase_value({
-        let ui_handle = ui.as_weak();
-        move || {
-            let ui = ui_handle.unwrap();
-            ui.set_counter(ui.get_counter() + 1);
-        }
-    });
+    // ui.on_request_increase_value({
+    //     let ui_handle = ui.as_weak();
+    //     move || {
+    //         let ui = ui_handle.unwrap();
+    //         ui.set_counter(ui.get_counter() + 1);
+    //     }
+    // });
+
     // let app = App::new().unwrap();
-    // let text = "Hello, world!".to_string();
+    let text = "Hello, world!".to_string();
     // app.set_text(text);
 
-
     ui.run()
-    
-    
-    
 }
 
-
 // 读取文件内容
-// fn read_content(s: &str) -> Result<Vec::<String>> {
-//     let v = Vec::new();
-//     Ok(v.push("Hello, world!".to_string()))
+
+// fn read_content(s: &str) -> Result<Vec<String>> {
+//     let mut v = Vec::new();
+//     v.push("Hello, world!".to_string());
+//     Ok(v)
 // }
