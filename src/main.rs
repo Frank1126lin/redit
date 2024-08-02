4b2201270a842d2f44c16fc13b8c7f25eb8e5847
frank <frank1126.lin@gmail.com>
2024-08-02 19:42:37 +0800

    add prev next page button

--- src/main.rs
+++ src/main.rs
@@ -23,17 +23,42 @@ fn main() -> Result<(), slint::PlatformError> {
     // logging(non_blocking);
     // 根据输入的文档路径，加载文档内容，并写入到编辑器中
     let ui = AppWindow::new()?;
-    let text = read_content("/home/andy/Downloads/2024.07.18.09.27.46_log.txt").unwrap();
-    // let mut i = 0;
+    let text = read_content("log.txt").unwrap();
+
     ui.on_show_text({
         let ui_handle = ui.as_weak();
         move || {
             let ui = ui_handle.unwrap();
-            let i = ui.get_page_num() as usize;
+            let mut i = ui.get_page_num();
+            if i < 0 {
+                i = 0;
+            }
+            let i = i as usize;
+            println!("page_num: {}", i);
             let txt: String = text[i * 1024..(i + 1) * 1024].join("\n");
             ui.set_content(txt.into());
         }
     });
+    // ui.on_prev_page({
+    //     let ui_handle = ui.as_weak();
+    //     move || {
+    //         let ui = ui_handle.unwrap();
+    //         let i = ui.get_page_num() as usize;
+    //         println!("page_num: {}", i);
+    //         let txt: String = &text[i * 1024..(i + 1) * 1024].join("\n").clone_into();
+    //         ui.set_content(txt.into());
+    //     }
+    // });
+    // ui.on_next_page({
+    //     let ui_handle = ui.as_weak();
+    //     move || {
+    //         let ui = ui_handle.unwrap();
+    //         let i = ui.get_page_num() as usize;
+    //         println!("page_num: {}", i);
+    //         let txt: &String = &text[i * 1024..(i + 1) * 1024].join("\n");
+    //         ui.set_content(txt.into());
+    //     }
+    // });
 
     ui.run()
 }
@@ -41,9 +66,6 @@ fn main() -> Result<(), slint::PlatformError> {
 // 读取文件内容
 fn read_content(s: &str) -> Result<Vec::<String>> {
     // 打开文件并读取内容
-    // let mut file = File::open(s)?;
-    // let mut content = String::new();
-    // file.read_to_string(&mut content)?;
     let content: String = fs::read_to_string(s)?;
     // 按行分割内容
     let v: Vec<String> = content.split("\n").map(|s| s.to_string()).collect();
